749adfb067645a72971af959042211b8fa5d8ef7
ET <10930255@dcsh.tp.edu.tw>
2020-11-12 10:36:45 +0800

    投票主題列表

--- default/models.py
+++ default/models.py
@@ -6,10 +6,15 @@ class Poll(models.Model):
     description= models.TextField()
     date_created=models.DateField(auto_now_add=True)
 
-  
+    def __str__(self):
+        return str(self.id) + ")" + self.subject 
+
 
 
 class Option(models.Model):
     poll_id = models.IntegerField()
     title = models.CharField(max_length=200)
     count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.id) + ")" + self.title + "@" + str(self.poll_id)
